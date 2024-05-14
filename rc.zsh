3b2a1a50f2ef7ea17eb6be99b7a0167a0940c83e
Pollux <hand.joseph+pollux@protonmail.com>
2024-05-14 13:14:08 -0500

    Add history search

--- rc.zsh
+++ rc.zsh
@@ -4,7 +4,14 @@ source "${0:A:h}/conda.zsh"
 source "${0:A:h}/pollux/init.zsh"
 
 PLUGIN_DIR="${0:A:h}/plugins"
+
+autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+zvm_after_init_commands+=("bindkey -a 'k' up-line-or-beginning-search" "bindkey -a 'j' down-line-or-beginning-search")
 source "$PLUGIN_DIR/zsh-vi-mode/zsh-vi-mode.zsh"
+
+
 source "$PLUGIN_DIR/zsh-async/async.zsh"
 
 autoload -Uz compinit promptinit
@@ -18,16 +25,15 @@ zstyle ':completion:*' menu select
 zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
 zstyle ':completion:*:warnings' format 'Sorry, no matches for: %d%b'
 
-export HISTSIZE=2000
-export HISTFILE="$HOME/.history"
-export SAVEHIST=$HISTSIZE
 
 setopt hist_ignore_all_dups
 setopt hist_ignore_space
-
-
 setopt autocd
 setopt extendedglob
+
+export HISTSIZE=2000
+export HISTFILE="$HOME/.history"
+export SAVEHIST=$HISTSIZE
 
 export GPG_TTY="$(tty)"
 export EDITOR="nvim"
