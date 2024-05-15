3b45f692fd0fe95beb9607784282331f174488ca
Pollux <hand.joseph+pollux@protonmail.com>
2024-05-15 13:20:52 -0500

    Rework nvim-remote binding

--- pollux/nvim.zsh
+++ pollux/nvim.zsh
@@ -1,14 +1,9 @@
 export EDITOR=nvim
 
-nvim ()
-{
- if [ -z "$NVIM" ]; then
-    /usr/bin/nvim $@
+nvim () {
+  if [ -z "$@" ]; then
+    nvr -c 'enew'
   else
-    if [ -z "$@" ]; then
-      nvr --servername $NVIM -c 'enew'
-    else
-      nvr --servername $NVIM --remote $@
-    fi
+    nvr $@
   fi
 }
